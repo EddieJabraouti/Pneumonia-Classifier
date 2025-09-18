597688aa1e94a44998630e4fc02b6d462eaec25c
eddiejab <1jabraoutie@gmail.com>
2025-09-18 14:01:16 -0400

    front end updates

--- frontend/pages/AnimatedHero.jsx
+++ frontend/pages/AnimatedHero.jsx
@@ -1,15 +1,39 @@
+import { clear } from 'console';
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const AnimatedHero = () => {
     const headingText = "Clarity";
     const [displayedText, setDisplayedText]= useState(" ");
-    const[showUser, setShowUser] = useState(true); 
+    const [showUser, setShowUser] = useState(true);
+    const [showCursor, setShowShowCursor] = useState(false); 
+    const [fadeInDesc, setFadeInDescription] = useState(false); 
+    const [fadeInButtons, setFadeInButtons] = useState(false);
 
+    useEffect = (() => { 
+        let i = 0; 
+        const typingTimer = setInterval(() => { 
+            if(i < headingText.length) { 
+                setDisplayedText(prev => prev + headingText.charAt(i)); 
+                i++
+            } else { 
+                clearInterval(typingTimer); 
+                setFadeInDescription(true); 
 
+                setTimeout(() => { 
+                    setFadeInButtons(true); 
+
+                    setTimeout(() => { 
+                        setShowShowCursor(false); 
+                    }, 500);
+                },500);
+            }
+        }, 100); 
+        return () => clearInterval(typingTimer);
+    }, []); 
   return (
-    <></>
+    <div>AnimatedHero</div>
   )
 }
 
--- frontend/pages/Model.jsx
+++ frontend/pages/Model.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Toolbar from '../components/Toolbar';
 import { Link } from 'react-router-dom';
 
 
