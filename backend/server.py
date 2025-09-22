620b95599a6ab7c6cc4fb187b1f052bfac88a9ea
eddiejab <1jabraoutie@gmail.com>
2025-09-22 01:00:08 -0400

    created virtual env for python backend

--- backend/server.py
+++ backend/server.py
@@ -1,20 +1,32 @@
-import os 
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import torch
-import torch.nn as nn
+# import os 
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import torch
+# import torch.nn as nn
 
-from model.nn import device, train_dataset, val_dataset, test_dataset, num_workers
-from torch.utils.data import DataLoader
+# from model.nn import device, train_dataset, val_dataset, test_dataset, num_workers
+# from torch.utils.data import DataLoader
 
+
+# app = Flask(__name__)
+# CORS(app, resources={r"/*":{"origins": "*"}})
+
+# @app.route("/Model", methods=['GET', 'POST'])
+# async def model_route(): 
+#     return "Dummy words till i implement properly...."
+
+# if __name__ == "__main__":
+#     print("starting Flask server...")
+#     app.run(host="0.0.0.0", port=5001, debug=True)
+
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app, resources={r"/*":{"origins": "*"}})
 
-@app.route("/Model", methods=['GET', 'POST'])
-async def model_route(): 
-    return "Dummy words till i implement properly...."
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello from Flask backend!"})
 
-if __name__ == "_main__":
-    print("starting Flask server...")
-    app.run(host="0.0.0.0", port=5001, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
