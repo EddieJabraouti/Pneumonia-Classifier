5d5cc30f1b489027f82666a2e4d4129b3f8e2994
eddiejab <1jabraoutie@gmail.com>
2025-09-24 13:25:14 -0400

    Model page changes

--- frontend/pages/Model.jsx
+++ frontend/pages/Model.jsx
@@ -9,12 +9,26 @@ import { Link } from 'react-router-dom';
 // DL models classification(including a sort of confidence score). refere to PrimePrep/lib/actions/general.actions.ts  
 // and PrimePrep/constants/index.ts
 
+//We are already getting a prediction in server.py, we just need to make function that gets this prediction and uses it here. 
+//
 
 const Model = () => {
 
+  const [showImage, setShowImage] = useState(false); //For when the image is shown or not
+  const [desc, setShowDesc] = useState(""); //For the analysis
+
+
 
   return (
-    <div>Model</div>
+    <div>
+      <div>
+        <h1 className='text-center'>Get a classification and elaborate analysis on your own image</h1>
+        <p className='text-center'>AI Powered Analysis</p>
+      </div>
+      <div className='rounded-2xl border border-gray-200 bg-white shadow-sm'>
+
+      </div>
+    </div>
   )
 }
 
